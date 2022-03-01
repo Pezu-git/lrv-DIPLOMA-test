0bff33e25244db01f76ba832c5abb5c2dd988683
Pezu-git <artem_nikolaev69@mail.ru>
2022-03-01 18:33:56 +0300

    fix

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -50,8 +50,7 @@ class MovieController extends Controller
      */
     public function destroy(Request $request)
     {
-        $movie_id = Movie::where('title', $request->title)->first()->id;
-        Movie::where('id', $movie_id)->first()->delete();
+        $movie_id = Movie::where('title', $request->title)->delete();
         MovieSchedule::where('movie_id', $movie_id)->delete();
     }
 }
