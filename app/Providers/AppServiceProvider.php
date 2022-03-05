32e30a0a048ca2dfd77ea49d5ccfa7fd73f4f08a
Pezu-git <artem_nikolaev69@mail.ru>
2022-03-05 09:50:30 +0300

    MVC

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Movie;
+use App\Models\Hall;
+use App\Models\Seat;
+use App\Models\HallConf;
+use App\Models\MovieSchedule;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $halls = Hall::all();
+        $movies = Movie::all();
+        $hallConf = HallConf::all();
+        View::share('movies', $movies);
+        View::share('hallCon', $hallConf);
     }
 }
